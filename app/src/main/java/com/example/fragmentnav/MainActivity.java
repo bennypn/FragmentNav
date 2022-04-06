ab3104a55369965866ae179a80f8a17b0169663d
bennyp <53509098+ultrabeney@users.noreply.github.com>
2022-04-06 10:33:35 +0700

    Initial commit

---
+++ app/src/main/java/com/example/fragmentnav/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.fragmentnav;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.example.fragmentnav.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()){
+
+                case R.id.Home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.Profile:
+                    replaceFragment(new ProfileFragment());
+                    break;
+                case R.id.Setting:
+                    replaceFragment(new SettingFragment());
+                    break;
+            }
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.FrameLayout,fragment);
+        fragmentTransaction.commit();
+
+    }
+}
